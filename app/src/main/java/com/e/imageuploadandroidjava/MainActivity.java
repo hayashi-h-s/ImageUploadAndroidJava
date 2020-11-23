ffcc0161d28c02649fd78a4228d6662b2fabf3a9
hayamizu_shinji <hayamizu.hnut@gmail.com>
2020-11-24 08:13:47 +0900

    add

---
+++ app/src/main/java/com/e/imageuploadandroidjava/MainActivity.java
@@ -0,0 +1,82 @@
+package com.e.imageuploadandroidjava;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int RESULT_PICK_IMAGEFILE = 1000;
+    private ImageView imageView;
+
+    // 参考 https://qiita.com/yamikoo@github/items/fe27482954cd73e55935
+    // Android端末内の画像をアップロード https://qiita.com/yamikoo@github/items/7bcfdf7f154f37c07e0d
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        imageView = (ImageView)findViewById(R.id.image_view);
+
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+
+                // 結果の画像を取得するため、startActivityForResultで起動 。第2引数には任意のID。
+                startActivityForResult(intent, RESULT_PICK_IMAGEFILE);
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+        if (requestCode == RESULT_PICK_IMAGEFILE && resultCode == RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+
+                uri = resultData.getData();
+
+                try {
+                    Bitmap bmp = getBitmapFromUri(uri);
+                    imageView.setImageBitmap(bmp);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        assert parcelFileDescriptor != null;
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+
+}
